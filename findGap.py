a787e271b6e4b407a779bc91e8114010a2ecb88d
ruoyu1123 <ruoyu1123@outlook.com>
2024-04-09 23:01:21 +0800

    main

--- findGap.py
+++ findGap.py
@@ -4,27 +4,22 @@ from scipy.sparse import coo_matrix
 
 def creat_matrix(input):
     '''
-    Read Hi-C sparse martix from a text file with the format is 
-    1 3 130 
+    Read Hi-C sparse martix from a text file with the format is
+    1 3 130
     2 2 500
     2 3 231
     The sparse will be switch to dense matrix in this function.
-    '''                      
+    '''
     data = np.loadtxt(input, delimiter="\t", dtype=int)
     sparse_matrix = coo_matrix((data[:, 2], (data[:, 0], data[:, 1])))
     dense_matrix = sparse_matrix.toarray()
     return dense_matrix
 
-def calc_10_DDQ(matrix):
-    '''
-    DDQ: Degrees Density Q-value
-    In this step, the DDQ of consecutive 10 nodes will be calculated.
-    '''
-    pass
-
 
 
 def findGap(matrix, threshold):
+    # This function is finding Candidates for Centremeres and they will be return
+    # with dict.
     degrees = []
     # Calculate degrees of nodes
     for i in range(4, len(matrix)-3):
@@ -32,10 +27,10 @@ def findGap(matrix, threshold):
         for j in range(i-3, i):
             ddq += matrix[j][i]
         for j in range(i,i+3):
-            ddq += matrix[i][j]        
+            ddq += matrix[i][j]
         degrees.append(ddq)
     avg = np.mean(degrees)
-    print(degrees, avg)
+
     variance = []
     for i in degrees:
         variance.append((i-avg)/avg)
@@ -54,11 +49,24 @@ def findGap(matrix, threshold):
                 errToler[singal] += 1
                 gap [singal] += 1
             else:
+                k = i-1
+                while -variance[k] <= threshold:
+                    gap[singal] -= 1
+                    errToler[singal] -= 1
+                    k -= 1
                 singal = None
-    print(gap)
-    print(errToler)
+    candidates = {key+4: value for key, value in gap.items()}
+    return candidates
+
+
+def findPosition(gap, finerMatrix):
+    pass
+
 
 if __name__ == "__main__":
-    input = "../hicpro/matrix/chr1.matrix"
+    input = "chr1.matrix"
     matrix = creat_matrix(input)
-    findGap(matrix, 0.7)
+    # for i in range(1, len(matrix)):
+    #     print(matrix[i][i])
+    gap = findGap(matrix, 0.7)
+    print(gap)
